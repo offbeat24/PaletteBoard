0b55202918498086ee6849814daba1521f143650
offbeat24 <77655547+offbeat24@users.noreply.github.com>
2023-03-15 17:32:27 +0900

    feat: grid-layout ing...

--- palette-board/src/DashBoard/Board.jsx
+++ palette-board/src/DashBoard/Board.jsx
@@ -1,12 +1,11 @@
+import GridLayout from "react-grid-layout";
 import React from 'react';
-import "./Board.scss";
 
-function board() {
-  return(
-    <div>
-      
-    </div>
-  )
-}
+const layout = [
+  { i: "blue-eyes-dragon", x: 0, y: 0, w: 1, h: 1 },
+  { i: "dark-magician", x: 1, y: 0, w: 1, h: 1 },
+  { i: "kuriboh", x: 2, y: 0, w: 1, h: 1 },
+  { i: "spell-caster", x: 3, y: 0, w: 1, h: 1 },
+  { i: "summoned-skull", x: 4, y: 0, w: 1, h: 1 }
+];
 
-export default board;
---
+++ palette-board/src/DashBoard/layout.js
@@ -0,0 +1,9 @@
+const layout = [
+  { i: "1", x: 0, y: 0, w: 1, h: 1 },
+  { i: "2", x: 1, y: 0, w: 1, h: 1 },
+  { i: "3", x: 2, y: 0, w: 1, h: 1 },
+  { i: "4", x: 3, y: 0, w: 1, h: 1 },
+  { i: "5", x: 4, y: 0, w: 1, h: 1 }
+];
+
+export default layout;
